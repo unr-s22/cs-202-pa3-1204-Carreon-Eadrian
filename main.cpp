dacb1b6f9e3a068a4ac5c437b38dde6b6c4e6958
enivera <enivera1@nevada.unr.edu>
2022-03-30 22:52:00 -0700

    fd

--- main.cpp
+++ main.cpp
@@ -34,17 +34,60 @@ int main()
 	std::cout << "This is the initial balance" << std::endl;
 	std::cout << account << std::endl;
 	
-	std::cout << "This is some overload checks currently check1 and check2" << std::endl;
-	//checks for money operator overloads
-	std::cout << depositOne+depositTwo << std::endl;
-	std::cout << depositOne-depositTwo << std::endl;
-	
 	//It says we need code that proves our relational operator overloads worked but I don't really know what to do for that. Like... is it related to the functionality at all or can we literally just print anything? I dunno
 	if (depositOne < depositTwo)
 	{
-		std::cout << "Hi" << std::endl;
+		std::cout << "Deposit one < Deposit two" << std::endl;
+	}
+	else
+	{
+		std::cout << "Deposit one is not smaller than Deposite two" << std::endl;
+	}
+	
+	if (depositThree > depositTwo)
+	{
+		std::cout << "Deposit three > Deposit two" << std::endl;
+	}
+	else
+	{
+		std::cout << "Deposit three is not larger than Deposit two" << std::endl;
+	}
+	
+	if (depositOne <= withdrawlOne)
+	{	
+		std::cout << "Deposit one <= withdrawal one" << std::endl;
+	}
+	else 
+	{
+		std::cout << "Deposit one is greater than withdrawal one" << std::endl;
+	}
+	
+	if (depositTwo >= withdrawlTwo)
+	{
+		std::cout << "Deposit two >= withdrawal two" << std::endl;
+	}
+	else
+	{
+		std::cout << "Deposit two is less than withdrawal two" << std::endl;
+	}
+	
+	if (depositOne == depositThree)
+	{
+		std::cout << "Deposit one == Deposit three" << std::endl;
+	}
+	else
+	{
+		std::cout << "The deposits are not equal" << std::endl;
 	}
 
+	if (withdrawlOne != withdrawlTwo)
+	{
+		std::cout << "Withdrawal one != Withdrawal two" << std::endl;
+	}
+	else
+	{
+		std::cout << "The withdrawals are equal" << std::endl;
+	}
 
 	return 0;
 	
